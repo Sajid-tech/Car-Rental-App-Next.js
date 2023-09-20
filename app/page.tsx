0d97c5e0d0372ac39664761bb4a719fdf83826c7
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2023-09-20 15:37:31 +0530

    Hero section done

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+"use client"
+import Hero from '@/components/home/Hero'
 import Image from 'next/image'
 
 
 export default function Home() {
   return (
-   <div>
-    <h1>Subscribe</h1>
+   <div className='p-5 sm:px-10 md:px-20'>
+    <Hero/>
    </div>
   )
 }
