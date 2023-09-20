20dbfa7a5a9544b20b6d7115fc8e2d0eb9e73cb2
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2023-09-20 17:56:07 +0530

    make Search Input

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import Hero from '@/components/home/Hero'
+import SearchInput from '@/components/home/SearchInput'
 import Image from 'next/image'
 
 
@@ -7,6 +8,7 @@ export default function Home() {
   return (
    <div className='p-5 sm:px-10 md:px-20'>
     <Hero/>
+    <SearchInput/>
    </div>
   )
 }
